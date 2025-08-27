63c94d85a7906dd174d5f8be318a7d9085312378
Shakyo <shakyochoudhury4@gmail.com>
2025-08-27 17:38:34 +0530

    Update settings.py
    removed tokens

--- settings.py
+++ settings.py
@@ -29,13 +29,13 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_v2env$wv_z6gay4q-xzwr#3w8%l84#nj=(@fr*dt230s&z-o@'
+SECRET_KEY = 'add your own key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # dont use DEBUG = true in production and initialize the url in ALLOWED_HOSTS       
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.heroku.com', '.pythonanywhere.com]
 # '127.0.0.1',  'codeitwithshakyo.herokuapp.com'
 
 # Application definition
@@ -174,3 +174,4 @@ SOCIAL_AUTH_FACEBOOK_SECRET = 'kys if u want something'
 SOCIAL_AUTH_TWITTER_KEY = 'lol'
 SOCIAL_AUTH_TWITTER_SECRET = 'aint no way'
 ID='lol skill issue'
+
